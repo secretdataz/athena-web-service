bfa8b0f0d72a18b41f80cc862db9a7898f305c39
Jittapan Pluemsumran <secret@jittapan.app>
2020-06-14 18:33:44 +0700

    Revert "Adjusted verification"
    This reverts commit cd6a963b52b513d93f228f23a4e4db641a7a16f3.

--- app/Auth/AuthTokenVerifier.php
+++ app/Auth/AuthTokenVerifier.php
@@ -10,17 +10,11 @@ class AuthTokenVerifier
     static function verify(int $accountId, string $authToken, int $guildId = 0): bool
     {
 		Log::info("Trying to verify AID:$accountId Token:$authToken GID:$guildId");
-
-        $result = DB::table('login')->where('login.account_id', $accountId)
+        return DB::table('login')->where('login.account_id', $accountId)->where('login.web_auth_token', $authToken)
                 ->when($guildId !== 0, function (Builder $q) use ($guildId) {
                     return $q->join('char', 'login.account_id', '=', 'char.account_id')
                         ->join('guild', 'guild.char_id', '=', 'char.char_id')
                         ->where('guild.guild_id', $guildId);
-                })->first();
-        if ($result && hash_equals($result->web_auth_token, $authToken)) {
-            return true;
-        }
-
-        return false;
+                })->count() > 0;
     }
 }
