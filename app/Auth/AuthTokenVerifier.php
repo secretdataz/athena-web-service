2a3a3e73e86c596e6fb5c3d15bf41fb819e5b278
Jittapan Pluemsumran <secret@jittapan.app>
2020-06-15 22:37:42 +0700

    Added IP checking feature

--- app/Auth/AuthTokenVerifier.php
+++ app/Auth/AuthTokenVerifier.php
@@ -6,9 +6,12 @@ use Illuminate\Support\Facades\DB;
 
 class AuthTokenVerifier
 {
-    static function verify(int $accountId, string $authToken, int $guildId = 0): bool
+    static function verify(int $accountId, string $authToken, int $guildId = 0, bool $forceSameIp = true, string $ip = '0.0.0.0'): bool
     {
         return DB::table('login')->where('login.account_id', $accountId)->where('login.web_auth_token', $authToken)
+                ->when($forceSameIp, function (Builder $q) use ($ip) {
+                    return $q->where('last_ip', $ip);
+                })
                 ->when($guildId !== 0, function (Builder $q) use ($guildId) {
                     return $q->join('char', 'login.account_id', '=', 'char.account_id')
                         ->join('guild', 'guild.char_id', '=', 'char.char_id')
--- config/athena.php
+++ config/athena.php
@@ -3,4 +3,5 @@
 return [
     'error_response' => '{"Type": 3}',
     'allowed_worlds' => ['rAthena'],
+    'force_same_ip' => env('ATHENA_FORCE_SAME_IP', true),
 ];
